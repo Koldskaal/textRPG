0d5f66be75c61b771f0ee6de8769b381f048cb08
Zaygone <gammatt@gmail.com>
2019-01-03 11:26:44 +0100

    input not working for some reason

--- Board.py
+++ Board.py
@@ -24,16 +24,50 @@ def position(board):
 position(board)
 print_board(board)
 
-#move_up = (raw_input("Move up: Type w"))
-move_up = 1
-if move_up == 1:
+move_up = (input("Move up: Type w"))
+#move_up = w #for testing purpose
+if move_up == w:
     player = board[xx][yy] = " "
     xx = xx-1
     def position(board):
-        player = board[xx][yy] = "I"
+        player = board[xx][yy] = "O"
         return xx
         return yy
 else:
     pass
+move_down = (input("Move up: Type s"))
+#move_down = s #for testing purpose
+if move_up == s:
+    player = board[xx][yy] = " "
+    xx = xx+1
+    def position(board):
+        player = board[xx][yy] = "O"
+        return xx
+        return yy
+else:
+    pass
+move_left = (input("Move up: Type a"))
+#move_let = a #for testing purpose
+if move_left == a:
+    player = board[xx][yy] = " "
+    yy = yy-1
+    def position(board):
+        player = board[xx][yy] = "O"
+        return xx
+        return yy
+else:
+    pass
+move_right = (input("Move up: Type d"))
+#move_right = d #for testing purpose
+if move_up == d:
+    player = board[xx][yy] = " "
+    yy = yy+1
+    def position(board):
+        player = board[xx][yy] = "O"
+        return xx
+        return yy
+else:
+    pass
+
 position(board)
 print_board(board)
