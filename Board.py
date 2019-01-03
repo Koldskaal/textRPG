1d7acd7962643f96471ccc9a8cad06ba0dbb2f99
Zaygone <gammatt@gmail.com>
2019-01-03 11:20:58 +0100

    board with type movement. Needs testing with raw_input outside of consol

--- Board.py
+++ Board.py
@@ -1,7 +1,7 @@
 board = []
 
-length = 10 #set side length of the square
-hight = 7 #set hight of the square
+length = 9 #set side length of the square
+hight = 9 #set hight of the square
 for x in range(0,hight):
     board.append(["X"]*length)
 for x in range(1,len(board)-1):
@@ -15,4 +15,25 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
 
+xx = int(length/2)
+yy = int(hight/2)
+def position(board):
+    player = board[xx][yy] = "O"
+    return xx
+    return yy
+position(board)
+print_board(board)
+
+#move_up = (raw_input("Move up: Type w"))
+move_up = 1
+if move_up == 1:
+    player = board[xx][yy] = " "
+    xx = xx-1
+    def position(board):
+        player = board[xx][yy] = "I"
+        return xx
+        return yy
+else:
+    pass
+position(board)
 print_board(board)
