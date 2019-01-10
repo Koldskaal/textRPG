937ca0890c02a028bc60346c760e14b48748911c
Aviv Tokman <avivtokman@gmail.com>
2019-01-10 11:33:43 +0100

    restructured textures

--- data/textures.py
+++ data/textures.py
@@ -1,24 +1,31 @@
 from termcolor import colored
 import os
 
+
+use_unicode = True
+
+WALL_CHAR_UP_DOWN = colored('▒', 'yellow')
+WALL_CHAR_LEFT_RIGHT = colored('▒', 'yellow')
+PLAYER_CHAR = colored('■', 'white', attrs=['bold'])
+MONSTER_CHAR = colored('Θ', 'green', attrs=['bold'])
+DOOR_CHAR = colored('|', 'yellow', attrs=['bold'])
+DOOR_CHAR_OPEN_NEXT = colored('▐', 'yellow', attrs=['bold'])
+DOOR_CHAR_OPEN_PREV = colored('▌', 'yellow', attrs=['bold'])
+
+SHOP_STAND = colored('▲', 'cyan', attrs=['bold'])
+
+BORDER_UP_DOWN = '─'
+BORDER_LEFT_RIGHT = '│'
+BORDER_CORNER_UP_LEFT = '┌'
+BORDER_CORNER_UP_RIGHT = '┐'
+BORDER_CORNER_DOWN_LEFT = '└'
+BORDER_CORNER_DOWN_RIGHT = '┘'
+
+
 try:
-
-    WALL_CHAR_UP_DOWN = colored('▒', 'yellow')
-    WALL_CHAR_LEFT_RIGHT = colored('▒', 'yellow')
-    PLAYER_CHAR = colored('■', 'white', attrs=['bold'])
-    MONSTER_CHAR = colored('Θ', 'green', attrs=['bold'])
-    DOOR_CHAR = colored('|', 'yellow', attrs=['bold'])
-    DOOR_CHAR_OPEN_NEXT = colored('▐', 'yellow', attrs=['bold'])
-    DOOR_CHAR_OPEN_PREV = colored('▌', 'yellow', attrs=['bold'])
     print(PLAYER_CHAR)
-    SHOP_STAND = colored('▲', 'cyan', attrs=['bold'])
-
-    BORDER_UP_DOWN = '─'
-    BORDER_LEFT_RIGHT = '│'
-    BORDER_CORNER_UP_LEFT = '┌'
-    BORDER_CORNER_UP_RIGHT = '┐'
-    BORDER_CORNER_DOWN_LEFT = '└'
-    BORDER_CORNER_DOWN_RIGHT = '┘'
+    if not use_unicode:
+        raise UnicodeEncodeError('for switching', u'', 42, 43, 'to switch char set')
 except UnicodeEncodeError:
     WALL_CHAR_UP_DOWN = colored('x', 'yellow')
     WALL_CHAR_LEFT_RIGHT = colored('x', 'yellow')
